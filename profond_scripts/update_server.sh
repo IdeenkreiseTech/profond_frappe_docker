a569466bbb8f4dea25acad870152943155016c3f
Sagar <74648730+SagarIdeen@users.noreply.github.com>
2024-06-08 15:19:43 +0530

    Update update_server.sh

--- profond_scripts/update_server.sh
+++ profond_scripts/update_server.sh
@@ -4,7 +4,7 @@ TAG=1.0.0
 cd ../
 sudo docker login ghcr.io -u IdeenkreiseTech -p $git_password
 sudo docker pull ghcr.io/ideenkreisetech/faboolux_webapp/faboolux_webapp:$TAG
-sudo docker compose -f compose.yaml -f overrides/compose.noproxy.yaml -f overrides/compose.mariadb.yam -f overrides/compose.redis.yaml down
-sudo docker compose -f compose.yaml -f overrides/compose.noproxy.yaml -f overrides/compose.mariadb.yam -f overrides/compose.redis.yaml up -d
+sudo docker compose -f compose.yaml -f overrides/compose.noproxy.yaml -f overrides/compose.mariadb.yaml -f overrides/compose.redis.yaml down
+sudo docker compose -f compose.yaml -f overrides/compose.noproxy.yaml -f overrides/compose.mariadb.yaml -f overrides/compose.redis.yaml up -d
 sudo docker compose exec backend bench --site $site_name migrate
 sudo docker compose restart backend
