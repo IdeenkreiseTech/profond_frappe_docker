43e87149ec6719d8f99136e0db7cb801dc501c2b
Sagar <74648730+SagarIdeen@users.noreply.github.com>
2024-06-08 15:15:33 +0530

    Update create_server.sh

--- profond_scripts/create_server.sh
+++ profond_scripts/create_server.sh
@@ -7,7 +7,7 @@ TAG=1.0.0
 cd ../
 sudo docker login ghcr.io -u IdeenkreiseTech -p $git_password
 sudo docker ghcr.io/ideenkreisetech/profond_app/profond_app:$TAG
-sudo docker compose -f compose.yaml -f overrides/compose.noproxy.yaml -f overrides/compose.mariadb.yam -f overrides/compose.redis.yaml up -d
+sudo docker compose -f compose.yaml -f overrides/compose.noproxy.yaml -f overrides/compose.mariadb.yaml -f overrides/compose.redis.yaml up -d
 sudo docker compose exec backend bench new-site $site_name  --no-mariadb-socket --mariadb-root-password $db_password --admin-password $admin_password
 sudo docker compose exec backend bench --site $site_name install-app profond_app
 sudo docker compose exec backend bench --site $site_name set-config encryption_key $encryption_key
